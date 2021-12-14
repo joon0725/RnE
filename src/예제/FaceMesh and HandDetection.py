22b1e42e8fdbd1053a4b14e20595a695d4c48e0d
joon0725 <joon0725@gmail.com>
2021-12-15 01:18:09 +0900

    update

--- src/예제/FaceMesh and HandDetection.py
+++ src/예제/FaceMesh and HandDetection.py
@@ -4,8 +4,7 @@ import cv2
 
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
-right_hand_list = []
-left_hand_list = []
+hand_list = []
 cap = cv2.VideoCapture(0)
 cnt = 0
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
@@ -15,8 +14,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = holistic.process(image)
         if cnt % 25 == 0:
-            right_hand_list.append(results.right_hand_landmarks)
-            left_hand_list.append(results.left_hand_landmarks)
+            hand_list.append(results.left_hand_landmarks)
             print(f"{cnt/25} {results.face_landmarks}\n\n")
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
@@ -32,5 +30,4 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
 cap.release()
 cv2.destroyAllWindows()
-print(right_hand_list[8][0])
-print(left_hand_list)
+print(hand_list)
