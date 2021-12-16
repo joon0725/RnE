c4672e94507fa690a1c9432463f88b73a9130888
joon0725 <joon0725@gmail.com>
2021-12-16 23:19:01 +0900

    sudo pacman -Syyu

--- auto_exec.py
+++ auto_exec.py
@@ -1,2 +1,30 @@
 import os
-import 
+import discord
+
+bot = discord.Client()
+
+
+def ai(message):
+    os.system(message)
+
+
+@bot.event
+async def on_ready(): # 봇이 실행 준비가 되었을 때 행동할 것
+    print('Logged in as')
+    print(bot.user.name) # 클라이언트의 유저 이름을 출력합니다.
+    print(bot.user.id) # 클라이언트의 유저 고유 ID를 출력합니다.
+    # 고유 ID는 모든 유저 및 봇이 가지고있는 숫자만으로 이루어진 ID입니다.
+    print('------')
+
+
+@bot.event
+async def on_message(message): # 입력되는 메세지에서 찾기
+    if message.author == bot.user:  # 만약 메시지를 보낸 사람과 봇이 서로 같을 때
+        return
+
+    if message.author.bot:  # discord.User.bot 프로퍼티가 참일 때
+        return
+
+    await message.channel.send(ai(message.content))
+bot.run('OTIxMDQzMTMwNjcxODQ5NTMy.YbtKQg.KRsufEhRsf5fB9uXgXY3hLBnw10')
+
